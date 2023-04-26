5b2218b9ad408564f6435cd72bce6228f9f37736
Mahé Perrette <mahe.perrette@gmail.com>
2023-04-26 18:56:28 +0200

    job.sh (to modify) to create masks on the cluster

--- job.sh
+++ job.sh
@@ -1,7 +1,16 @@
 module load python/3.9.12
-module load_netcdf
+module load netcdf
 module load geos/3.9.1
 
 source venv/bin/activate
 
-sbatch geojson_to_grid.py --grid 5arcmin --fractional-mask --version v2.6
+#sbatch --mem=8000 geojson_to_grid.py --grid 0.5deg --binary-mask --fractional-mask --version v2.6
+#sbatch --mem=64000 geojson_to_grid.py --grid 5arcmin --binary-mask --fractional-mask --version v2.6
+#sbatch --mem=64000 geojson_to_grid.py --grid 30arcsec --binary-mask --version v2.6
+
+sbatch --mem=8000 geojson_to_grid.py --grid 0.5deg --fractional-mask --version v2.6
+sbatch --mem=64000 geojson_to_grid.py --grid 5arcmin --fractional-mask --version v2.6
+
+# and later:
+# mv countrymasks_fractional_0.5deg.nc countrymasks_fractional.nc
+# mv countrymasks_0.5deg.nc countrymasks.nc
